dd721fddc23e9da04f5ec6fd9ed677d8cb093ade
musta10 <mustafaenfadal@gmail.com>
2021-03-30 01:49:58 +0200

    creadochanelenladb

--- src/controllers/private.controller.js
+++ src/controllers/private.controller.js
@@ -1,9 +1,12 @@
 import Private from "../models/Private"
 
 
-export const createPrivate = (req, res) => {
-    console.log(req.body);
-    res.json('creating chanel')
+export const createPrivate = async (req, res) => {
+    // console.log(req.body);
+    const {name, description} = req.body
+    const newPrivate = new Private({name, description});
+    const privateSaved = await newPrivate.save()
+    res.status(201).json(privateSaved)
 }
 export const getPrivate = (req, res) => {
     res.json('obtener chanels')
