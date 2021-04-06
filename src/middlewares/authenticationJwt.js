6e0667ee29a4ddd15d40474533ae16760934a619
musta10 <mustafaenfadal@gmail.com>
2021-04-06 19:47:18 +0200

    encontrarusuario

--- src/middlewares/authenticationJwt.js
+++ src/middlewares/authenticationJwt.js
@@ -1,9 +1,18 @@
 import jwt from "jsonwebtoken"
+import config from "../config"
+import User from "../models/User";
 
 export const verifyToken = async (req,res,next) => {
 const token = req.headers["x-access-token"];
 console.log(token);
 
 if(!token) return res.status(403).json({message: "Token No Valido"})
+
+const decoded = jwt.verify(token, config.SECRET)
+
+const user = await User.findById(decoded.id, {password: 0})
+console.log(user);
+if(!user) return res.status(404).json({message: "usuario no existe"})
+
 next()
 }
