d37942878be075e5b71857ff46079ca439b89b65
musta10 <mustafaenfadal@gmail.com>
2021-04-07 01:26:53 +0200

    terminadotodousuarios

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,11 @@
-import {Router} from 'express';
-const router = Router()
+import { Router } from "express";
+const router = Router();
 
-import * as authCtrl from "../controllers/auth.controller"
+import * as authCtrl from "../controllers/auth.controller";
+import { verifySignup } from "../middlewares";
 
+router.post("/signup",[verifySignup.duplicateNameOrEmail, verifySignup.checkRolesExiste],authCtrl.signUp);
 
-router.post('/signup', authCtrl.signUp)
-
-router.post('/signin', authCtrl.signIn)
-
+router.post("/signin", authCtrl.signIn);
 
 export default router;
